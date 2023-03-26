04bdf14735e32521b8aba16e0acbad8564004cf4
Kamrul Hasan <kamrul444216@gmail.com>
2023-03-27 00:24:47 +0600

    card data lode and design

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -8,6 +8,13 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img className='img' src={img} alt="" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings}star</p>
+            </div>
+            <button className='btn-cart'>Add to cart</button>
         </div>
     );
 };
