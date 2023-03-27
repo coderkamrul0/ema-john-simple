0bab39ffa4ad8803ca83c8c6bc023fc08ae1ffa7
Kamrul Hasan <kamrul444216@gmail.com>
2023-03-28 02:31:06 +0600

    added from local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,17 +4,20 @@ const Cart = (props) => {
     const {cart} = props;
 
     let total = 0;
+    let quantity = 0;
     let totalShipping = 0;
     for(const product of cart){
-        total = total + product.price
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping
+        quantity = quantity+ product.quantity
     }
     const tax = total*7/100;
     const grandTotal = total+totalShipping+tax;
     return (
         <div className='cart'> 
             <h4>Order summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${total.toFixed(2)}</p>
             <p>Total Shipping: ${totalShipping.toFixed(2)}</p>
             <p>TAX: ${tax.toFixed(2)}</p>
